3f54d90af974e95f21b8fd3789812eb45ad85ba5
Michael Bruce <michael.bruce@singletracksystems.com>
2016-03-29 17:18:16 +0100

    created user_input method

--- navi.rb
+++ navi.rb
@@ -5,11 +5,23 @@ class Navi
     puts 'Hey Listen'
     token = read_config('.slacky')
     display_message('hi', {from: 'mike'})
+    while true
+      user_input
+    end
     #Slack.new(token).history('ok')[0..3].each { |message| puts message }
   end
 
   def display_message(message, details={})
     puts 'slack:mike # what\'s for lunch?'
+  end
+
+  def user_input
+    print 'mike => '
+    lastCommand = gets
+    puts "You entered, #{lastCommand}"
+    if lastCommand.chomp == 'exit'
+      exit
+    end
   end
 
   def read_config(config_name)
